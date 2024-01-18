4de320e65bad2cbbb69d1b5eaefa5b1e873c2200
Shiva Kant Singh <shivakant091@outlook.com>
2024-01-18 05:44:23 +0530

    Commit Ordering API

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,31 @@
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase<OrderContext>((context, services) =>
+                {
+                    var logger = services.GetService<ILogger<OrderContextSeed>>();
+                    OrderContextSeed
+                        .SeedAsync(context, logger)
+                        .Wait();
+                })
+                .Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
